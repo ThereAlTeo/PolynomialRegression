719f958f0f0241b5b1c3feb2e4863de40d8247ae
Matteo Alesiani <matteo.alesiani2@studio.unibo.it>
2019-06-28 18:17:39 +0200

    Inserimento di codice per analisi esplorativa

--- src/application.py
+++ src/application.py
@@ -1,16 +1,16 @@
 import csv
 import pandas as pd
 import os.path as fileSystem
+import matplotlib.pyplot as plot
 
 FILEPATH = "OnlineNewsPopularity.csv"
 
 #Zona del programma in cui vengono collocate le funzioni.
 #Esse verranno chiamate all'occorrenza all'interno del programma
 
-
 def loadCSVFile(path):
     if fileSystem.exists(path):
-        return pd.read_csv(path, sep=",")
+        return pd.read_csv(path, sep=",")#, dtype={x: "category" for x in range(30, 39)})
     else:
         print("File non trovato")
 
@@ -22,9 +22,23 @@ def generalDataFrameInfo(dataFrame):
 def getRelativePath():
     return fileSystem.dirname(fileSystem.dirname(__file__)) + "\\res\\datasheet\\"
 
+#La funzione describe tende ad escludere il primo attributo.
+#Probabilmente perchè di tipo object e quindi non ha competenze per il calcolo dei valori.
+def exploratoryAnalysis(dataFrame):
+    generalDataFrameInfo(dataFrame)
+    print(dataFrame.describe())
+
+    pd.cut(dataFrame[" rate_positive_words"], 4).value_counts().plot.pie()
+    plot.show()
+
 #INIZIO DEL PROGRAMMA
 
+#Da verificare il corretto utilizzo di datasheet.
+#Esso può essere richiamato e utilizzato dalla funzione, senza l'obbligo di essere passato alle funzioni stesse come argomento.
+#Può essere considerato con scope globale all'interno del progetto.
 datasheet = loadCSVFile(str(getRelativePath()) + str(FILEPATH))
 
-#print(datasheet)
-generalDataFrameInfo(datasheet)
+
+
+#ANALISI ESPLORATIVA
+exploratoryAnalysis(datasheet)
