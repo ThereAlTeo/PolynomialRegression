11597c04885d7f536c9c3effa10e81b52046e359
Matteo Alesiani <matteo.alesiani2@studio.unibo.it>
2019-07-02 00:43:10 +0200

    Proive di sviluppo con Perceptron.

--- src/application.py
+++ src/application.py
@@ -5,7 +5,7 @@ import os.path as fileSystem
 import matplotlib.pyplot as plot
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-from sklearn.linear_model import Lasso, LinearRegression
+from sklearn.linear_model import Lasso, LinearRegression, Ridge, ElasticNet, LogisticRegression, Perceptron
 from sklearn.model_selection import train_test_split
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.metrics import mean_squared_error
@@ -74,52 +74,108 @@ def exploratoryAnalysis(dataFrame):
     pd.cut(dataFrame["MaritalStatus"], 2).value_counts().plot.pie()
     plot.show()
 
-def elaborationWithoutLasso(XTrain, YTrain):
-    prm = Pipeline([("poly",   PolynomialFeatures(degree=2, include_bias=False)), #se viene fatta di terzo grado non basta la memoria
-                    ("scale",  StandardScaler()),   # <- aggiunto
-                    ("linreg", LinearRegression(normalize=True))])
+'''tre diversi metodi di elaborazione: regressione senza vincoli, Ridge e Lasso
+producono diversi modelli di previsione'''
+def elaborationWithRidge(XTrain, YTrain, dg):
+    prm = Pipeline([#("poly",   PolynomialFeatures(degree=dg, include_bias=False)), #se viene fatta di terzo grado non basta la memoria
+                    ("scaler",  StandardScaler()),   # <- aggiunto , n_jobs=-1  
+                    ("model",  Perceptron(penalty="l2", alpha=0.005, max_iter=10))
+                    ])
     prm.fit(XTrain, YTrain)
     return prm
+
+def elaborationWithElasticNetdef(XTrain, YTrain, dg):
+    prm = Pipeline([#("poly",   PolynomialFeatures(degree=dg, include_bias=False)), #se viene fatta di terzo grado non basta la memoria
+                     ("scale",  StandardScaler()),
+                     ("regr",  KernelRidge(alpha=10, kernel="poly", degree=1))])
+    prm.fit(XTrain, YTrain)
+    return prm
+
+def elaborationWithoutRestrain(XTrain, YTrain, dg):
+    prm = Pipeline([ ("poly",   PolynomialFeatures(degree=dg, include_bias=False)),
+                    ("scale",  StandardScaler()),   # <- aggiunto
+                    ("linreg", LinearRegression())])
+    prm.fit(XTrain, YTrain)
+    return prm
+
+def elaborationWithLasso(XTrain, YTrain, dg):
+    model = Pipeline([("poly", PolynomialFeatures(degree=dg, include_bias=False)),
+                    ("scale",  StandardScaler()),   # <- aggiunto
+                    ("linreg", Lasso(alpha=2))])
+    model.fit(XTrain, YTrain)
+    return model
+
+def plot_model_on_data(x, y, model=None):
+    plot.scatter(x, y)
+    if model is not None:
+        xlim, ylim = plot.gca().get_xlim(), plot.gca().get_ylim()
+        line_x = np.linspace(xlim[0], xlim[1], 100)
+        line_y = model.predict(line_x[:, None])
+        plot.plot(line_x, line_y, c="red", lw=3)
+        plot.xlim(xlim); plt.ylim(ylim)
+    plot.grid()
+    plot.xlabel("Temperatura (°C)"); plt.ylabel("Consumi (GW)")
+    plot.show()
 
 def relative_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true))
 
-def elaborationWithLasso(XTrain, YTrain):
-    model = Pipeline([("poly", PolynomialFeatures(degree=1, include_bias=False)),
-                    ("scale",  StandardScaler()),   # <- aggiunto
-                    ("linreg", Lasso(alpha=10))])
-    model.fit(XTrain, YTrain)
-    print(pd.Series(model.named_steps["linreg"].coef_, XTrain.columns))
-    #print(model.named_steps["linreg"].coef_)
-    #print(XTrain.columns)
+'''prototipo: l'idea è quella di utilizare un modello a Lasso di primo grado per determinare
+le variabili inutili, che vengono eliminate dal dataset.
+viene creato successivamente un'altro modello dal dataset modificato
+PS: migliora la previsione dal 36 al 34 % di errore ma non riesco a farlo fare in maniera automatica
+Restituisce in modo da rendere omogeneo il modello con i dati di test'''
+def multipleElaboration(XTrain, YTrain):
+    model = elaborationWithLasso(XTrain, YTrain, 1)
+    tmp = pd.Series(model.named_steps["linreg"].coef_, XTrain.columns)
+    print("....................................")
+    a = []
+    for row in tmp.index:
+        if(tmp[row]==0):
+            a.append(row)
+    XTrain = XTrain.drop(a, axis=1)
+    model = elaborationWithRidge(XTrain, YTrain, 1)
+    return model, a
 
-    return model
+
 
 def dataElaboration(dataFrame):
     Y = dataFrame["Purchase"].values
-    X = dataFrame.drop(["ProductCategory3","ProductCategory2", "Purchase"], axis=1)
+    X = dataFrame.drop(["ProductCategory3", "ProductCategory2", "Purchase"], axis=1)
     XTrain, XVal, YTrain, YVal = slipDataset(X, Y)
-    p = elaborationWithLasso(XTrain, YTrain)
-
-    #p = elaborationWithoutLasso(XTrain, YTrain)
+    #p = elaborationWithRidge(XTrain, YTrain, 2)
     #print(p)
+    p, a = multipleElaboration(XTrain, YTrain)
     #print(p.named_steps["linreg"].coef_)
-    #print(XTrain.columns)
+    print(XTrain.columns)
+    XVal = XVal.drop(a, axis=1)
     printEvalutation(XVal, YVal, p)
-    #print(pd.Series(p.named_steps["linreg"].coef_, XTrain.columns))
+    #plot_model_on_data(XVal, YVal, p)
+    '''
+    Non funziona ancora
+
+    plot.scatter(XVal["ProductCategory"], YVal),
+    line_x = XVal["ProductCategory1"]
+    line_y = p.predict(XVal);
+    plt.plot(line_x, line_y, c="red", lw=3)
+    '''
+
+
 
 def printEvalutation(X, Y, model):
     print("Mean squared error    : {:.5}".format(mean_squared_error(model.predict(X), Y)))
     print("Relative error        : {:.5%}".format(relative_error(model.predict(X), Y)))
     print("R-squared coefficient : {:.5}".format(model.score(X, Y)))
 
+
 def slipDataset(X, Y):
-    return train_test_split(X, Y, test_size=1/10, random_state=73)
+    return train_test_split(X, Y, test_size=0.33, random_state=73)
 
 def main():
     dataset = loadCSVFile(str(getRelativePath()) + str(FILEPATH))
     dataset.set_index(["UserID", "ProductID"], inplace=True)
     dataset = binarizza(dataset)
+    #print(dataset)
     #exploratoryAnalysis(dataset)
     dataElaboration(dataset)
 
